ec0d7ea4fd45593d58b0178beb7c296612a45f73
Abhishek Shubham <abhishekshubham79@gmail.com>
2025-12-08 17:43:27 +0530

    Added DELETE endpoint to remove posts by ID with async DB handling and error responses

--- app/app.py
+++ app/app.py
@@ -89,5 +89,26 @@ async def get_feed(
     return posts_data
 
 
+@app.delete("/post/{post_id}")
+
+async def delete_post(
+    post_id: str,
+    session: AsyncSession = Depends(get_async_session)
+):  
+    try:
+        result= await session.execute(select(Post).where(Post.id == post_id))
+        post = result.scalars().first()
+
+        if not post: 
+            raise HTTPException(status_code=404, detail="Post not found")
+        
+        await session.delete(post)
+        await session.commit()
+        return {"detail": "Post deleted successfully"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
 
     
